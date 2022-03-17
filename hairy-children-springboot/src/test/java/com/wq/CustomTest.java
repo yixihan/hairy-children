21c7ec9a012cbeb5bff10160602aec43d7d73622
yixihan <yixihan20010617@gmail.com>
2022-03-18 00:32:51 +0800

    完成登录界面

---
+++ hairy-children-springboot/src/test/java/com/wq/CustomTest.java
@@ -0,0 +1,15 @@
+package com.wq;
+
+import org.junit.Test;
+
+/**
+ * @author : yixihan
+ * @create : 2022-03-17-23:32
+ */
+public class CustomTest {
+
+    @Test
+    public void test () {
+
+    }
+}
